dfd35e45c6be9d44625717a880f3ea8b882c18d8
Maxim Holovin <jumpstart1982@gmail.com>
2026-02-27 16:52:14 +0200

    neon database connected

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  datasource: {
-    // Тепер ми не пишемо пароль тут, а беремо його з .env
-    url: process.env.DATABASE_URL,
-  },
-});
